e80c30b9bf10b5dff8775e680fd871997a65fcb1
k3v1n888 <40829114+k3v1n888@users.noreply.github.com>
2025-06-08 10:44:47 +0800

    Update ProtectedRoutes.js

--- frontend/src/components/ProtectedRoutes.js
+++ frontend/src/components/ProtectedRoutes.js
@@ -1,4 +1,4 @@
-// src/ProtectedRoutes.js
+// frontend/src/components/ProtectedRoutes.js
 
 import React, { useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
@@ -6,6 +6,8 @@ import { UserContext } from "../context/UserContext";
 
 export default function ProtectedRoutes() {
   const { user } = useContext(UserContext);
+
+  console.log("ProtectedRoutes: current user state:", user); // ADD THIS
 
   if (!user) {
     // If not logged in, send to the React “/login” page

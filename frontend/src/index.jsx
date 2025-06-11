fdd5d7f7e33cf74faf4495a947e4f56a577ebe86
k3v1n888 <40829114+k3v1n888@users.noreply.github.com>
2025-06-11 14:45:54 +0800

    Update index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { UserProvider } from './context/UserContext';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* --- THIS IS THE FIX: Router must be the outer component --- */}
-    <Router>
-      <UserProvider>
-        <App />
-      </UserProvider>
-    </Router>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
